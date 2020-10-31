6dcac7c38b89fd67c943f361c495e56326359597
Phil Gresham <philgresham@gmail.com>
2020-10-30 22:29:57 -0700

    Updated tests for NavbarTitle

--- src/tests/NavbarTitle.test.jsx
+++ src/tests/NavbarTitle.test.jsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { getByTestId, render } from '@testing-library/react';
+import {
+  // act,
+  // screen,
+  getByTestId,
+  render as rtlRender,
+} from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import NavbarTitle from '../components/atoms/NavbarTitle';
 
 describe('NavbarTitle atom', () => {
-  render(
-    <BrowserRouter>
-      <NavbarTitle />
-    </BrowserRouter>,
-  );
+  const render = (ui, { route = '/new-round' } = {}) => {
+    window.history.pushState({}, 'Test page', route);
+
+    return rtlRender(ui, { wrapper: BrowserRouter });
+  };
+  render(<NavbarTitle />);
 
   const navbarTitle = getByTestId(document.documentElement, 'navbar-title');
   test('renders in the document', () => {
@@ -18,4 +25,12 @@ describe('NavbarTitle atom', () => {
   test('has text content', () => {
     expect(navbarTitle).toHaveTextContent(/\w/);
   });
+
+  // // Don't know how to do this best....
+  // test('routes to home page when clicked', () => {
+  //   act(() => {
+  //     userEvent.click(navbarTitle.parentNode);
+  //   });
+  //   expect(location.pathname).toBe('/');
+  // });
 });
